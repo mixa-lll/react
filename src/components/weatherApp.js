8f2b406a50d2ebf35b7b5ddc821d50cec62446e5
mixa-lll <mihail@MacBook-Pro-Mihail.local>
2021-03-05 17:09:05 +0300

    init

---
+++ src/components/weatherApp.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+  
+class WeatherApp extends Component{
+    constructor(){
+        super();
+        this.state = {
+            weatherData: null
+          };
+    }
+    componentDidMount(){
+        const zip = this.props.zip;
+        const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+          zip +
+          "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+        fetch(URL).then(res => res.json()).then(json => {
+          this.setState({ weatherData: json });
+        });
+    }
+    render(){
+
+            const weatherData = this.state.weatherData;
+            if (!weatherData) return <div>Loading</div>;
+            const weather = weatherData.weather[0];
+            const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+return (
+        <div>
+            <h1 className="weatherTitle">
+            {weather.main} in {weatherData.name}
+            <img src={iconUrl} alt={weatherData.description} />
+            </h1>
+            <p>Current: {Math.floor((weatherData.main.temp - 32) /1.8)}°</p>
+            <p>High: {Math.floor(( weatherData.main.temp_max - 32) /1.8)}°</p>
+            <p>Low: {Math.floor(( weatherData.main.temp_min - 32) /1.8)}°</p>
+            <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+        </div>
+)
+    }
+}
+export default WeatherApp;
